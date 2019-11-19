b1ad40f78c519775c1a2b1763301320d4f8cd939
silvioguiso <silvio.guiso@gmail.com>
2019-11-18 16:30:11 -0800

    Added photopoint model

--- PhotoPoints/app/src/main/java/edu/cascadia/mobas/photopoints/model/PhotoPoint.java
+++ PhotoPoints/app/src/main/java/edu/cascadia/mobas/photopoints/model/PhotoPoint.java
@@ -1,4 +1,53 @@
 package edu.cascadia.mobas.photopoints.model;
 
 public class PhotoPoint {
+
+    public enum PhotoPointType{
+        Creek, //0
+        Plant  //1
+    }
+
+    private String mPhotoPointID;
+    private Double mLatitude;
+    private Double mLongitude;
+    private PhotoPointType mPhotoPointType;
+
+    public PhotoPoint(String photoPointID, Double latitude, Double longitude, PhotoPointType mPhotoPointType) {
+        this.mPhotoPointID = photoPointID;
+        this.mLatitude = latitude;
+        this.mLongitude = longitude;
+        this.mPhotoPointType = mPhotoPointType;
+    }
+
+    public String getPhotoPointID() {
+        return mPhotoPointID;
+    }
+
+    public void setPhotoPointID(String photoPointID) {
+        this.mPhotoPointID = photoPointID;
+    }
+
+    public Double getLatitude() {
+        return mLatitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.mLatitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return mLongitude;
+    }
+
+    public void setmLongitude(Double longitude) {
+        this.mLongitude = longitude;
+    }
+
+    public PhotoPointType getPhotoPointType() {
+        return mPhotoPointType;
+    }
+
+    public void setPhotoPointType(PhotoPointType photoPointType) {
+        this.mPhotoPointType = photoPointType;
+    }
 }
