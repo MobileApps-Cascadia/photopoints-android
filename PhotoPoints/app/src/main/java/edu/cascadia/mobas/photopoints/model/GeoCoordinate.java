6137cc614729d9d4e16141c009acaf3f21181b41
Stephen Gomez-Fox <gomezfox@uw.edu>
2020-02-13 14:33:00 -0800

    Updates GeoCoordinate class for compatibility, all usages will be migrated to Coordinates

--- PhotoPoints/app/src/main/java/edu/cascadia/mobas/photopoints/model/GeoCoordinate.java
+++ PhotoPoints/app/src/main/java/edu/cascadia/mobas/photopoints/model/GeoCoordinate.java
@@ -6,25 +6,32 @@ import com.google.android.gms.maps.model.LatLng;
 
 
 public class GeoCoordinate {
-    private static double mMinLatitude = -90;
-    private static double mMaxLatitude = 90;
-    private static double mMinLongitude = -180.0;
-    private static double mMaxLongitude = 180.0;
+    private static final double mMinLatitude = -90;
+    private static final double mMaxLatitude = 90;
+    private static final double mMinLongitude = -180.0;
+    private static final double mMaxLongitude = 180.0;
+    private static final double mMinAltitude = 0;
 
-    private double mLatitude = 0;
     private double mLongitude = 0;
+    private double mLatitude = 0;
+    private double mAltitude = 0;
     private boolean mIsValid = false;
 
 
     // constructors
 
-    public GeoCoordinate(double lat, double lng) {
+    public GeoCoordinate(double lat, double lng, double alt) {
         if (GeoCoordinate.mMinLatitude <= lat && lat <= GeoCoordinate.mMaxLatitude
                 && GeoCoordinate.mMinLongitude <= lng && lng <= mMaxLongitude) {
             mLatitude = lat;
             mLongitude = lng;
+            mAltitude = alt;
             mIsValid = true;
         }
+    }
+
+    public GeoCoordinate(double lat, double lng) {
+        this(lat, lng, 0);
     }
 
     // constructor:  create from string
@@ -33,7 +40,9 @@ public class GeoCoordinate {
         String [] coordSet = coords.trim().split(",", 4);
         double lng = Double.parseDouble(coordSet[0]);
         double lat = Double.parseDouble(coordSet[1]);
-        // swap coordinates if order given is longitude,latitude
+        double alt = (coordSet.length < 3 ? 0 : Double.parseDouble(coordSet[2]));
+
+        // swap coordinates if order given is longitude,latitude (based on North America)
         if (lat < 0 && lng > 0) {
             double temp = lat;
             lat = lng;
@@ -44,44 +53,39 @@ public class GeoCoordinate {
                 && GeoCoordinate.mMinLongitude <= lng && lng <= mMaxLongitude) {
             mLatitude = lat;
             mLongitude = lng;
+            mAltitude = alt;
             mIsValid = true;
         }
     }
 
-
     // return the latitude
-
-    public double latitude() {
+    public double getLatitude() {
         return mLatitude;
     }
 
 
     // return the longitude
-
-    public double longitude() {
+    public double getLongitude() {
         return mLongitude;
     }
 
-    // return the coordinate as a Maps API LatLng
 
+    // return the altitude
+    public double getAltitude() { return mAltitude; }
+
+
+    // return the coordinate as a Maps API LatLng
     public LatLng getLatLng() {
         return new LatLng(mLatitude, mLongitude);
     }
 
+    // set the latitude
+    public void setLatitude(long lat) { this.mLatitude = lat; }
 
-    // return true if coordinate is valid and in bounds
-    public boolean isValid() {
-        return mIsValid;
-        }
+    // set the longitude
+    public void setLongitude(long lng) { this.mLongitude = lng; }
 
-
-    // set the bounding box which determine valid range of lat/lng coordinates
-
-    public static void setBounds(double minLat, double minLng, double maxLat, double maxLng) {
-        GeoCoordinate.mMinLatitude = minLat;
-        GeoCoordinate.mMinLongitude = minLng;
-        GeoCoordinate.mMaxLatitude = maxLat;
-        GeoCoordinate.mMaxLongitude = maxLng;
-    }
+    // set the altitude
+    public void setAltutude(long alt) { this.mAltitude = alt; }
 
 }
