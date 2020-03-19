c4f20899d421e4d3cb31943867a71315d8d3d6f9
Stephen Gomez-Fox <gomezfox@uw.edu>
2020-03-19 15:59:25 -0700

    Allows database to use UI thread

--- PhotoPoints/app/src/main/java/edu/cascadia/mobas/photopoints/data/PhotoPointsDatabase.java
+++ PhotoPoints/app/src/main/java/edu/cascadia/mobas/photopoints/data/PhotoPointsDatabase.java
@@ -33,7 +33,7 @@ import edu.cascadia.mobas.photopoints.model.PointImage;
 
 
 //For now, we can set the ExportSchema flag to false. We might want to set this to true later if we want to start using migrations.
-@Database(entities = {DBUser.class, DBPointItem.class, DBPointImage.class, DBPlant.class}, version = 1, exportSchema = false)
+@Database(entities = {DBUser.class, DBPointItem.class, DBPointImage.class, DBPlant.class}, version = 3, exportSchema = false)
 @TypeConverters({TimeStampConverter.class})
 public abstract class PhotoPointsDatabase extends RoomDatabase {
 
@@ -77,6 +77,7 @@ public abstract class PhotoPointsDatabase extends RoomDatabase {
                         });
                     }
                 })
+                .allowMainThreadQueries()         // TODO: Use threads for all queries
                 .fallbackToDestructiveMigration()
                 .build();
     }
