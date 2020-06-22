00af425c76aacf787ed6a22749c92d8353170b30
gomezfox <gomezfox@uw.edu>
2020-06-22 16:21:23 -0700

    Adds equals() and hashCode() to ItemRecord

--- PhotoPoints/app/src/main/java/edu/cascadia/mobas/photopoints/data/dto/ItemRecord.java
+++ PhotoPoints/app/src/main/java/edu/cascadia/mobas/photopoints/data/dto/ItemRecord.java
@@ -4,6 +4,7 @@ import androidx.room.Embedded;
 import androidx.room.Relation;
 
 import java.util.List;
+import java.util.Objects;
 
 import edu.cascadia.mobas.photopoints.data.dto.DetailEntity;
 import edu.cascadia.mobas.photopoints.data.dto.ImageEntity;
@@ -11,9 +12,24 @@ import edu.cascadia.mobas.photopoints.data.dto.ItemEntity;
 
 public class ItemRecord {
     @Embedded
-    public ItemEntity imageEntity;
+    public ItemEntity itemEntity;
     @Relation(parentColumn = "id", entityColumn = "id", entity = DetailEntity.class)
     List<DetailEntity> detailEntityList;
     @Relation(parentColumn = "id", entityColumn = "id", entity = ImageEntity.class)
     List<ImageEntity> imageEntityList;
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof ItemRecord)) return false;
+        ItemRecord that = (ItemRecord) obj;
+        return itemEntity.equals(that.itemEntity) &&
+                detailEntityList.equals(that.detailEntityList) &&
+                imageEntityList.equals(that.imageEntityList);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(itemEntity, detailEntityList, imageEntityList);
+    }
 }
