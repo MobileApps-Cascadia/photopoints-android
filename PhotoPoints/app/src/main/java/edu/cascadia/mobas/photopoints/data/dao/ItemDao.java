0e9077763a33891bf7a816203603fa48989ac24c
gomezfox <gomezfox@uw.edu>
2020-06-22 15:35:35 -0700

    ItemDao supports DataSource.Factory<ItemRecord>

--- PhotoPoints/app/src/main/java/edu/cascadia/mobas/photopoints/data/dao/ItemDao.java
+++ PhotoPoints/app/src/main/java/edu/cascadia/mobas/photopoints/data/dao/ItemDao.java
@@ -6,13 +6,19 @@ import androidx.room.Insert;
 import androidx.room.Query;
 import androidx.room.Update;
 import java.util.List;
+
+import androidx.paging.DataSource;
+
 import edu.cascadia.mobas.photopoints.data.dto.ItemEntity;
 import edu.cascadia.mobas.photopoints.data.dto.ItemRecord;
 
 @Dao
 public interface ItemDao {
-    @Query("SELECT id, type, label, location FROM items where inactive = 0 ")
-    List<ItemRecord> getAllItemRecords();
+    //@Query("SELECT * FROM items where inactive = 0 ")
+    //List<ItemRecord> getAllItemRecords();
+
+    @Query("SELECT * FROM items where inactive = 0 ")
+    DataSource.Factory<Integer, ItemRecord> getAllItemRecords();
 
     @Insert
     void insert(ItemEntity itemEntity);
